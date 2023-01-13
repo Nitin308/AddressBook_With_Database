7ff82137a594f6f60cd0ee44bef9806fcd7af7ce
Nitin308 <nitin3081998@gmail.com>
2023-01-13 11:43:31 +0530

    Create AddressBookTable

---
+++ AddressBook.sql
@@ -0,0 +1,14 @@
+﻿---UC_1 Create a Address Book Service DB
+create database AddressBookService;
+use AddressBookService;
+
+---UC_2 create a AddressBook Table
+create table AddressBookTable(
+First_name varchar(255),
+Last_name varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+Zip int,
+Phone_number bigint,
+Email varchar(255));
